ca170ddd345ce84cf3494450635b937389641c75
oskr <hoscar161@gmail.com>
2019-04-05 23:27:24 -0600

    agrege modulo de conexion

---
+++ CooperativaXyz/CooperativaXyz/Conexion/Conexion.vb
@@ -0,0 +1,5 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Module Conexion
+    Public Cn As New SqlConnection("Data Source=NATHALIEPALMA;Initial Catalog=CarWashSystem;Integrated Security=True")
+End Module
